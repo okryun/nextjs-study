54b6b2d9310b364ad511bad8a5ca41cf8dbcf906
ok2 <memoria0124@gmail.com>
2025-04-30 18:47:25 +0900

    add file

--- app/create-account/page.tsx
+++ app/create-account/page.tsx
@@ -1,37 +1,59 @@
 "use client";
 
-import { ChatBubbleLeftEllipsisIcon } from "@heroicons/react/16/solid";
+import Button from "@/components/button";
+import Input from "@/components/input";
+import SocialLogin from "@/components/social-login";
 
-import Link from "next/link";
+import { createAccount } from "./actions";
+import { useActionState } from "react";
+
 export default function CreateAccount() {
+  const [state, dispatch] = useActionState(createAccount, null);
+
   return (
     <div className="flex flex-col gap-10 py-8 px-6">
       <div className="flex flex-col gap-2 *:font-medium">
         <h1 className="text-2xl">안녕하세요!</h1>
         <h2 className="text-xl">Fill in the form below to join!</h2>
       </div>
-      <form className="flex flex-col gap-3">
-        <div className="flex flex-col gap-2">
-          <input
-            className="bg-transparent rounded-md w-full h-10 focus:outline-none ring-1 focus:ring-2 ring-neutral-200 focus:ring-orange-500 border-none placeholder:text-neutral-400"
-            type="text"
-            placeholder="Username"
-            required
-          />
-          <span className="text-red-500 font-medium">Input error</span>
-        </div>
-        <button className="primary-btn h-10">Create account</button>
+      <form action={dispatch} className="flex flex-col gap-3">
+        <Input
+          name="username"
+          type="text"
+          placeholder="Username"
+          required
+          icon
+          errors={state?.fieldErrors.username}
+        />
+        <Input
+          name="email"
+          type="email"
+          placeholder="Email"
+          required
+          icon
+          errors={state?.fieldErrors.email}
+        />
+        <Input
+          name="password"
+          type="password"
+          placeholder="Password"
+          minLength={4}
+          required
+          icon
+          errors={state?.fieldErrors.password}
+        />
+        <Input
+          name="confirm_password"
+          type="password"
+          placeholder="Confirm Password"
+          required
+          minLength={4}
+          icon
+          errors={state?.fieldErrors.confirm_password}
+        />
+        <Button text="Create account" />
       </form>
-      <div className="w-full h-px bg-neutral-500" />
-      <div>
-        <Link
-          className="primary-btn flex h-10 items-center justify-center gap-2"
-          href="/sms"
-        >
-          <span>{<ChatBubbleLeftEllipsisIcon className="w-5 h-5" />}</span>
-          <span>Sign up with SMS</span>
-        </Link>
-      </div>
+      <SocialLogin />
     </div>
   );
 }
